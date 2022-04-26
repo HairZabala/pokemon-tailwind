93081a5e32c266423c8789e4d16cd837f607d4a6
Juan Manuel Palomino Wolff <jump720@gmail.com>
2022-04-27 02:20:15 +1000

    tailwindconfig

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -23,7 +23,7 @@ export const PokemonCard: FC<Props> = ({ pokemon }) => {
     //   {pokemon.name}
     // </div>
     <div onClick={handleClick}>
-      <div className="p-1 shadow-lg flex flex-col rounded-lg border border-gray-200 hover:scale-105 ease-in-out duration-700">
+      <div className="p-1 shadow-lg flex flex-col rounded-lg border border-gray-200 hover:scale-105 ease-in-out duration-300">
         <Image src={img} width={140} height={140} alt="" />
         <div className="mt-4 flex justify-between px-4">
           <div className="capitalize">{name}</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,43 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00bcd4",
+        secondary: "#ff9800",
+        DK: "#212121",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        "5xl": ["3rem", { lineHeight: "1.25", letterSpacing: "-0.02em" }],
+        "6xl": ["3.75rem", { lineHeight: "1.2", letterSpacing: "-0.02em" }],
+      },
+      screens: {
+        xs: "480px",
+      },
+      borderWidth: {
+        3: "3px",
+      },
+      minHeight: {
+        9: "9rem",
+        11: "11rem",
+      },
+      minWidth: {
+        36: "9rem",
+        44: "11rem",
+        56: "14rem",
+        60: "15rem",
+        80: "20rem",
+        150: "60rem",
+      },
+      maxWidth: {
+        "8xl": "88rem",
+        "9xl": "96rem",
+      },
+    },
   },
   plugins: [],
-}
+};
